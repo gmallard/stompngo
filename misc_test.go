e9fbfbfe530caa2d7484f6bdfe6326da6d8aa481
gmallard <allard.guy.m@gmail.com>
2012-05-26 15:09:12 -0400

    Add tests for corner cases, body lengths 0 and 1.

---
+++ misc_test.go
@@ -0,0 +1,121 @@
+//
+// Copyright © 2012 Guy M. Allard
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/*
+A STOMP 1.1 Compatible Client Library
+*/
+package stompngo
+
+import (
+	"testing"
+)
+
+// Test A zero Byte Message (Corner Case)
+func TestBytes0(t *testing.T) {
+	// Write phase
+	n, _ := openConn(t)
+	conn_headers := check11(TEST_HEADERS)
+	c, _ := Connect(n, conn_headers)
+	//
+	m := "" // No data
+	d := "/queue/zero.byte.msg"
+	h := Headers{"destination", d}
+	e := c.Send(h, m)
+	if e != nil {
+		t.Errorf("Expected nil error, got [%v]\n", e)
+	}
+	//
+	_ = c.Disconnect(empty_headers)
+	_ = closeConn(t, n)
+
+	// Read phase
+	n, _ = openConn(t)
+	conn_headers = check11(TEST_HEADERS)
+	c, _ = Connect(n, conn_headers)
+	//
+	h = h.Add("id", d)
+	s, e := c.Subscribe(h)
+	if e != nil {
+		t.Errorf("Expected no subscribe error, got [%v]\n", e)
+	}
+	if s == nil {
+		t.Errorf("Expected subscribe channel, got [nil]\n")
+	}
+	md := <-s // Read message data
+	if md.Error != nil {
+		t.Errorf("Expected no message data error, got [%v]\n", md.Error)
+	}
+
+	// The real tests here
+	if len(md.Message.Body) != 0 {
+		t.Errorf("Expected body length 0, got [%v]\n", len(md.Message.Body))
+	}
+	if string(md.Message.Body) != m {
+		t.Errorf("Expected [%v], got [%v]\n", m, string(md.Message.Body))
+	}
+	//
+	_ = c.Disconnect(empty_headers)
+	_ = closeConn(t, n)
+
+}
+
+// Test A One Byte Message (Corner Case)
+func TestBytes1(t *testing.T) {
+	// Write phase
+	n, _ := openConn(t)
+	conn_headers := check11(TEST_HEADERS)
+	c, _ := Connect(n, conn_headers)
+	//
+	m := "1" // Just one byte
+	d := "/queue/one.byte.msg"
+	h := Headers{"destination", d}
+	e := c.Send(h, m)
+	if e != nil {
+		t.Errorf("Expected nil error, got [%v]\n", e)
+	}
+	//
+	_ = c.Disconnect(empty_headers)
+	_ = closeConn(t, n)
+
+	// Read phase
+	n, _ = openConn(t)
+	conn_headers = check11(TEST_HEADERS)
+	c, _ = Connect(n, conn_headers)
+	//
+	h = h.Add("id", d)
+	s, e := c.Subscribe(h)
+	if e != nil {
+		t.Errorf("Expected no subscribe error, got [%v]\n", e)
+	}
+	if s == nil {
+		t.Errorf("Expected subscribe channel, got [nil]\n")
+	}
+	md := <-s // Read message data
+	if md.Error != nil {
+		t.Errorf("Expected no message data error, got [%v]\n", md.Error)
+	}
+
+	// The real tests here
+	if len(md.Message.Body) != 1 {
+		t.Errorf("Expected body length 1, got [%v]\n", len(md.Message.Body))
+	}
+	if string(md.Message.Body) != m {
+		t.Errorf("Expected [%v], got [%v]\n", m, string(md.Message.Body))
+	}
+	//
+	_ = c.Disconnect(empty_headers)
+	_ = closeConn(t, n)
+
+}
