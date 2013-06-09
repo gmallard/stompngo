f97de99c549088e8c60fdac315bda83cb6b24815
gmallard <allard.guy.m@gmail.com>
2013-06-09 08:24:36 -0400

    Refactor headers tests.

--- headers_test.go
+++ headers_test.go
@@ -47,7 +47,7 @@ func TestDataHeadersBasic(t *testing.T) {
 	//
 	h = Headers{k}
 	if e := h.Validate(); e != EHDRLEN {
-		t.Errorf("Header Unexpected value for Validate: [%v]\n", e)
+		t.Errorf("Header Validate, got [%v], expected [%v]\n", e, EHDRLEN)
 	}
 }
 
@@ -58,21 +58,29 @@ func TestDataHeadersUTF8(t *testing.T) {
 	k := "keya"
 	v := "valuea"
 	h := Headers{k, v}
-	if _, e := h.ValidateUTF8(); e != nil {
+	var e error
+	var s string
+	if s, e = h.ValidateUTF8(); e != nil {
 		t.Errorf("Unexpected UTF8 error 1: [%v]\n", e)
+	}
+	if s != "" {
+		t.Errorf("Unexpected UTF8 error 1B, got [%v], expected [%v]\n", s, "")
 	}
 	//
 	h = Headers{k, v, `“Iñtërnâtiônàlizætiøn”`, "valueb", "keyc", `“Iñtërnâtiônàlizætiøn”`}
-	if _, e := h.ValidateUTF8(); e != nil {
+	if _, e = h.ValidateUTF8(); e != nil {
 		t.Errorf("Unexpected UTF8 error 2: [%v]\n", e)
 	}
 	//
 	h = Headers{k, v, `“Iñtërnâtiônàlizætiøn”`, "\x80", "keyc", `“Iñtërnâtiônàlizætiøn”`}
-	if _, e := h.ValidateUTF8(); e == nil {
+	if s, e = h.ValidateUTF8(); e == nil {
 		t.Errorf("Unexpected UTF8 error  3, got nil, expected an error")
-		if e != EHDRUTF8 {
-			t.Errorf("Unexpected UTF8 error 4, got [%v], expected [%v]\n", e, EHDRUTF8)
-		}
+	}
+	if e != EHDRUTF8 {
+		t.Errorf("Unexpected UTF8 error 4, got [%v], expected [%v]\n", e, EHDRUTF8)
+	}
+	if s != "\x80" {
+		t.Errorf("Unexpected UTF8 error 5, got [%v], expected [%v]\n", s, "\x80")
 	}
 }
 
@@ -95,16 +103,16 @@ func TestDataHeadersAddDelete(t *testing.T) {
 	hb := Headers{"kaa", "va", "kbb", "vb", "kcc", "vc"}
 	hn := ha.AddHeaders(hb)
 	if len(ha)+len(hb) != len(hn) {
-		t.Errorf("Unexpected length AddHeaders, expected: [%v], got: [%v]\n", len(ha)+len(hb), len(hn))
+		t.Errorf("Unexpected length AddHeaders, got: [%v], expected: [%v]\n", len(hn), len(ha)+len(hb))
 	}
 	ol := len(hn)
 	hn = hn.Delete("ka")
 	if len(hn) != ol-2 {
-		t.Errorf("Unexpected length Delete 1, expected: [%v], got: [%v]\n", ol-2, len(hn))
+		t.Errorf("Unexpected length Delete 1, got: [%v], expected: [%v]\n", len(hn), ol-2)
 	}
 	hn = hn.Delete("kcc")
 	if len(hn) != ol-4 {
-		t.Errorf("Unexpected length Delete 2, expected: [%v], got: [%v]\n", ol-4, len(hn))
+		t.Errorf("Unexpected length Delete 2, got: [%v], expected: [%v]\n", len(hn), ol-4)
 	}
 }
 
@@ -115,11 +123,11 @@ func TestDataHeadersContainsKV(t *testing.T) {
 	ha := Headers{"ka", "va", "kb", "vb", "kc", "vc"}
 	b := ha.ContainsKV("kb", "vb")
 	if !b {
-		t.Errorf("KV01 Expected true, got false")
+		t.Errorf("KV01 got false, expected true")
 	}
 	b = ha.ContainsKV("kb", "zz")
 	if b {
-		t.Errorf("KV02 Expected false, got true")
+		t.Errorf("KV02 got true, expected false")
 	}
 }
 
@@ -157,13 +165,13 @@ func TestDataHeadersSize(t *testing.T) {
 	s := ha.Size(false)
 	var w int64 = 4
 	if s != w {
-		t.Errorf("SIZ01 Expected size [%d], got [%d]\n", w, s)
+		t.Errorf("SIZ01 size, got [%d], expected [%v]\n", s, w)
 	}
 	//
 	ha = Headers{"kaa", "vaa2", "kba", "vba2", "kca", "vca2"}
 	s = ha.Size(true)
 	w = 3 + 1 + 4 + 1 + 3 + 1 + 4 + 1 + 3 + 1 + 4 + 1
 	if s != w {
-		t.Errorf("SIZ02 Expected size [%d], got [%d]\n", w, s)
+		t.Errorf("SIZ02 size, got [%d] expected [%v]\n", s, w)
 	}
 }
