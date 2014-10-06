8a67ec97931be165e01350a3d0b7bc22ac038301
gmallard <allard.guy.m@gmail.com>
2014-10-06 14:31:23 -0400

    Do not close main input channel.

--- disconnect.go
+++ disconnect.go
@@ -70,6 +70,5 @@ func (c *Connection) Disconnect(h Headers) error {
 	// Receipt
 	c.DisconnectReceipt = <-c.input
 	c.log(DISCONNECT, "end", ch, c.DisconnectReceipt)
-	close(c.input) // close message data channel
 	return nil
 }
