42ac5b12334f9f171ba2a86d56f5c52f594a95bb
gmallard <allard.guy.m@gmail.com>
2016-07-04 22:21:34 -0400

    Bump version to v1.0.2.

--- version.go
+++ version.go
@@ -32,7 +32,7 @@ var (
 	pref  = "v"       // Prefix
 	major = "1"       // Major
 	minor = "0"       // Minor
-	patch = "1.p.006" // Patch
+	patch = "2"       // Patch
 )
 
 func Version() string {
