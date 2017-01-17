5f9db7828b835459ffc34cbe0524c350e50cba39
Guy M. Allard <allard.guy.m@gmail.com>
2017-01-17 14:42:49 -0500

    Rework miscellaneous tests.

--- misc_test.go
+++ misc_test.go
@@ -24,200 +24,210 @@ import (
 	Test A zero Byte Message, a corner case.
 */
 func TestMiscBytes0(t *testing.T) {
-	// Write phase
-	n, _ = openConn(t)
-	ch := check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	ms := "" // No data
-	d := tdest("/queue/misc.zero.byte.msg")
-	sh := Headers{HK_DESTINATION, d}
-	e = conn.Send(sh, ms)
-	if e != nil {
-		t.Fatalf("Expected nil error, got [%v]\n", e)
-	}
-	//
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
+	for _, sp := range Protocols() {
+		// Write phase
+		n, _ = openConn(t)
+		ch := login_headers
+		ch = headersProtocol(ch, sp)
+		conn, _ = Connect(n, ch)
+		//
+		ms := "" // No data
+		d := tdest("/queue/misc.zero.byte.msg")
+		sh := Headers{HK_DESTINATION, d}
+		e = conn.Send(sh, ms)
+		if e != nil {
+			t.Fatalf("Expected nil error, got [%v]\n", e)
+		}
+		//
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 
-	// Read phase
-	n, _ = openConn(t)
-	ch = check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	sbh := sh.Add(HK_ID, d)
-	sc, e = conn.Subscribe(sbh)
-	if e != nil {
-		t.Fatalf("Expected no subscribe error, got [%v]\n", e)
-	}
-	if sc == nil {
-		t.Fatalf("Expected subscribe channel, got [nil]\n")
-	}
+		// Read phase
+		n, _ = openConn(t)
+		ch = login_headers
+		ch = headersProtocol(ch, sp)
+		conn, _ = Connect(n, ch)
+		//
+		sbh := sh.Add(HK_ID, d)
+		sc, e = conn.Subscribe(sbh)
+		if e != nil {
+			t.Fatalf("Expected no subscribe error, got [%v]\n", e)
+		}
+		if sc == nil {
+			t.Fatalf("Expected subscribe channel, got [nil]\n")
+		}
 
-	// Read MessageData
-	var md MessageData
-	select {
-	case md = <-sc:
-	case md = <-conn.MessageData:
-		t.Fatalf("read channel error:  expected [nil], got: [%v]\n",
-			md.Message.Command)
-	}
+		// Read MessageData
+		var md MessageData
+		select {
+		case md = <-sc:
+		case md = <-conn.MessageData:
+			t.Fatalf("read channel error:  expected [nil], got: [%v]\n",
+				md.Message.Command)
+		}
 
-	if md.Error != nil {
-		t.Fatalf("Expected no message data error, got [%v]\n", md.Error)
-	}
+		if md.Error != nil {
+			t.Fatalf("Expected no message data error, got [%v]\n", md.Error)
+		}
 
-	// The real tests here
-	if len(md.Message.Body) != 0 {
-		t.Fatalf("Expected body length 0, got [%v]\n", len(md.Message.Body))
+		// The real tests here
+		if len(md.Message.Body) != 0 {
+			t.Fatalf("Expected body length 0, got [%v]\n", len(md.Message.Body))
+		}
+		if string(md.Message.Body) != ms {
+			t.Fatalf("Expected [%v], got [%v]\n", ms, string(md.Message.Body))
+		}
+		//
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 	}
-	if string(md.Message.Body) != ms {
-		t.Fatalf("Expected [%v], got [%v]\n", ms, string(md.Message.Body))
-	}
-	//
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
-
 }
 
 /*
 	Test A One Byte Message, a corner case.
 */
 func TestMiscBytes1(t *testing.T) {
-	// Write phase
-	n, _ = openConn(t)
-	ch := check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	ms := "1" // Just one byte
-	d := tdest("/queue/one.byte.msg")
-	sh := Headers{HK_DESTINATION, d}
-	e = conn.Send(sh, ms)
-	if e != nil {
-		t.Fatalf("Expected nil error, got [%v]\n", e)
-	}
-	//
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
+	for _, sp := range Protocols() {
+		// Write phase
+		n, _ = openConn(t)
+		ch := login_headers
+		ch = headersProtocol(ch, sp)
+		conn, _ = Connect(n, ch)
+		//
+		ms := "1" // Just one byte
+		d := tdest("/queue/one.byte.msg")
+		sh := Headers{HK_DESTINATION, d}
+		e = conn.Send(sh, ms)
+		if e != nil {
+			t.Fatalf("Expected nil error, got [%v]\n", e)
+		}
+		//
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 
-	// Read phase
-	n, _ = openConn(t)
-	ch = check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	sbh := sh.Add(HK_ID, d)
-	sc, e = conn.Subscribe(sbh)
-	if e != nil {
-		t.Fatalf("Expected no subscribe error, got [%v]\n", e)
-	}
-	if sc == nil {
-		t.Fatalf("Expected subscribe channel, got [nil]\n")
-	}
+		// Read phase
+		n, _ = openConn(t)
+		ch = login_headers
+		ch = headersProtocol(ch, sp)
+		conn, _ = Connect(n, ch)
+		//
+		sbh := sh.Add(HK_ID, d)
+		sc, e = conn.Subscribe(sbh)
+		if e != nil {
+			t.Fatalf("Expected no subscribe error, got [%v]\n", e)
+		}
+		if sc == nil {
+			t.Fatalf("Expected subscribe channel, got [nil]\n")
+		}
 
-	// Read MessageData
-	var md MessageData
-	select {
-	case md = <-sc:
-	case md = <-conn.MessageData:
-		t.Fatalf("read channel error:  expected [nil], got: [%v]\n",
-			md.Message.Command)
-	}
+		// Read MessageData
+		var md MessageData
+		select {
+		case md = <-sc:
+		case md = <-conn.MessageData:
+			t.Fatalf("read channel error:  expected [nil], got: [%v]\n",
+				md.Message.Command)
+		}
 
-	if md.Error != nil {
-		t.Fatalf("Expected no message data error, got [%v]\n", md.Error)
-	}
+		if md.Error != nil {
+			t.Fatalf("Expected no message data error, got [%v]\n", md.Error)
+		}
 
-	// The real tests here
-	if len(md.Message.Body) != 1 {
-		t.Fatalf("Expected body length 1, got [%v]\n", len(md.Message.Body))
+		// The real tests here
+		if len(md.Message.Body) != 1 {
+			t.Fatalf("Expected body length 1, got [%v]\n", len(md.Message.Body))
+		}
+		if string(md.Message.Body) != ms {
+			t.Fatalf("Expected [%v], got [%v]\n", ms, string(md.Message.Body))
+		}
+		//
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 	}
-	if string(md.Message.Body) != ms {
-		t.Fatalf("Expected [%v], got [%v]\n", ms, string(md.Message.Body))
-	}
-	//
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
-
 }
 
 /*
 	Test nil Headers.
 */
 func TestMiscNilHeaders(t *testing.T) {
-	n, _ = openConn(t)
-	//
-	_, e = Connect(n, nil)
-	if e == nil {
-		t.Fatalf("Expected [%v], got [nil]\n", EHDRNIL)
-	}
-	if e != EHDRNIL {
-		t.Fatalf("Expected [%v], got [%v]\n", EHDRNIL, e)
-	}
-	//
-	ch := check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	e = nil // reset
-	e = conn.Abort(nil)
-	if e == nil {
-		t.Fatalf("Abort Expected [%v], got [nil]\n", EHDRNIL)
-	}
-	//
-	e = nil // reset
-	e = conn.Ack(nil)
-	if e == nil {
-		t.Fatalf("Ack Expected [%v], got [nil]\n", EHDRNIL)
-	}
-	//
-	e = nil // reset
-	e = conn.Begin(nil)
-	if e == nil {
-		t.Fatalf("Begin Expected [%v], got [nil]\n", EHDRNIL)
-	}
-	//
-	e = nil // reset
-	e = conn.Commit(nil)
-	if e == nil {
-		t.Fatalf("Commit Expected [%v], got [nil]\n", EHDRNIL)
-	}
-	//
-	e = nil // reset
-	e = conn.Disconnect(nil)
-	if e == nil {
-		t.Fatalf("Disconnect Expected [%v], got [nil]\n", EHDRNIL)
-	}
-	//
-	if conn.Protocol() > SPL_10 {
+	for _, _ = range Protocols() {
+		n, _ = openConn(t)
+		//
+		_, e = Connect(n, nil)
+		if e == nil {
+			t.Fatalf("Expected [%v], got [nil]\n", EHDRNIL)
+		}
+		if e != EHDRNIL {
+			t.Fatalf("Expected [%v], got [%v]\n", EHDRNIL, e)
+		}
+		//
+		ch := check11(TEST_HEADERS)
+		conn, _ = Connect(n, ch)
+		//
+		e = nil // reset
+		e = conn.Abort(nil)
+		if e == nil {
+			t.Fatalf("Abort Expected [%v], got [nil]\n", EHDRNIL)
+		}
+		//
+		e = nil // reset
+		e = conn.Ack(nil)
+		if e == nil {
+			t.Fatalf("Ack Expected [%v], got [nil]\n", EHDRNIL)
+		}
+		//
+		e = nil // reset
+		e = conn.Begin(nil)
+		if e == nil {
+			t.Fatalf("Begin Expected [%v], got [nil]\n", EHDRNIL)
+		}
+		//
+		e = nil // reset
+		e = conn.Commit(nil)
+		if e == nil {
+			t.Fatalf("Commit Expected [%v], got [nil]\n", EHDRNIL)
+		}
+		//
 		e = nil // reset
 		e = conn.Disconnect(nil)
 		if e == nil {
-			t.Fatalf("Nack Expected [%v], got [nil]\n", EHDRNIL)
+			t.Fatalf("Disconnect Expected [%v], got [nil]\n", EHDRNIL)
 		}
+		//
+		if conn.Protocol() > SPL_10 {
+			e = nil // reset
+			e = conn.Disconnect(nil)
+			if e == nil {
+				t.Fatalf("Nack Expected [%v], got [nil]\n", EHDRNIL)
+			}
+		}
+		//
+		e = nil // reset
+		e = conn.Send(nil, "")
+		if e == nil {
+			t.Fatalf("Send Expected [%v], got [nil]\n", EHDRNIL)
+		}
+		//
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 	}
-	//
-	e = nil // reset
-	e = conn.Send(nil, "")
-	if e == nil {
-		t.Fatalf("Send Expected [%v], got [nil]\n", EHDRNIL)
-	}
-	//
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
 }
 
 /*
 Test max function.
 */
 func TestMiscMax(t *testing.T) {
-	var l int64 = 1 // low
-	var h int64 = 2 // high
-	mr := max(l, h)
-	if mr != 2 {
-		t.Fatalf("Expected [%v], got [%v]\n", h, mr)
-	}
-	mr = max(h, l)
-	if mr != 2 {
-		t.Fatalf("Expected [%v], got [%v]\n", h, mr)
+	for _, _ = range Protocols() {
+		var l int64 = 1 // low
+		var h int64 = 2 // high
+		mr := max(l, h)
+		if mr != 2 {
+			t.Fatalf("Expected [%v], got [%v]\n", h, mr)
+		}
+		mr = max(h, l)
+		if mr != 2 {
+			t.Fatalf("Expected [%v], got [%v]\n", h, mr)
+		}
 	}
 }
 
@@ -225,12 +235,14 @@ func TestMiscMax(t *testing.T) {
 Test hasValue function.
 */
 func TestMiscHasValue(t *testing.T) {
-	sa := []string{"a", "b"}
-	if !hasValue(sa, "a") {
-		t.Fatalf("Expected [true], got [false] for [%v]\n", "a")
-	}
-	if hasValue(sa, "z") {
-		t.Fatalf("Expected [false], got [true] for [%v]\n", "z")
+	for _, _ = range Protocols() {
+		sa := []string{"a", "b"}
+		if !hasValue(sa, "a") {
+			t.Fatalf("Expected [true], got [false] for [%v]\n", "a")
+		}
+		if hasValue(sa, "z") {
+			t.Fatalf("Expected [false], got [true] for [%v]\n", "z")
+		}
 	}
 }
 
@@ -238,12 +250,14 @@ func TestMiscHasValue(t *testing.T) {
 Test Uuid function.
 */
 func TestMiscUuid(t *testing.T) {
-	id := Uuid()
-	if id == "" {
-		t.Fatalf("Expected a UUID, got empty string\n")
-	}
-	if len(id) != 36 {
-		t.Fatalf("Expected a 36 character UUID, got length [%v]\n", len(id))
+	for _, _ = range Protocols() {
+		id := Uuid()
+		if id == "" {
+			t.Fatalf("Expected a UUID, got empty string\n")
+		}
+		if len(id) != 36 {
+			t.Fatalf("Expected a 36 character UUID, got length [%v]\n", len(id))
+		}
 	}
 }
 
@@ -251,47 +265,49 @@ func TestMiscUuid(t *testing.T) {
 	Test Bad Headers
 */
 func TestMiscBadHeaders(t *testing.T) {
-	//
-	n, _ = openConn(t)
-	neh := Headers{"a", "b", "c"} // not even number header count
-	conn, e = Connect(n, neh)
-	if e == nil {
-		t.Fatalf("Expected [%v], got [nil]\n", EHDRLEN)
+	for _, _ = range Protocols() {
+		//
+		n, _ = openConn(t)
+		neh := Headers{"a", "b", "c"} // not even number header count
+		conn, e = Connect(n, neh)
+		if e == nil {
+			t.Fatalf("Expected [%v], got [nil]\n", EHDRLEN)
+		}
+		if e != EHDRLEN {
+			t.Fatalf("Expected [%v], got [%v]\n", EHDRLEN, e)
+		}
+		//
+		bvh := Headers{HK_HOST, "localhost", HK_ACCEPT_VERSION, "3.14159"}
+		conn, e = Connect(n, bvh)
+		if e == nil {
+			t.Fatalf("Expected [%v], got [nil]\n", EBADVERCLI)
+		}
+		if e != EBADVERCLI {
+			t.Fatalf("Expected [%v], got [%v]\n", EBADVERCLI, e)
+		}
+		//
+		ch := check11(TEST_HEADERS)
+		conn, e = Connect(n, ch) // Should be a good connect
+		//
+		_, e = conn.Subscribe(neh)
+		if e == nil {
+			t.Fatalf("Expected [%v], got [nil]\n", EHDRLEN)
+		}
+		if e != EHDRLEN {
+			t.Fatalf("Expected [%v], got [%v]\n", EHDRLEN, e)
+		}
+		//
+		e = conn.Unsubscribe(neh)
+		if e == nil {
+			t.Fatalf("Expected [%v], got [nil]\n", EHDRLEN)
+		}
+		if e != EHDRLEN {
+			t.Fatalf("Expected [%v], got [%v]\n", EHDRLEN, e)
+		}
+		//
+		if conn != nil && conn.Connected() {
+			_ = conn.Disconnect(empty_headers)
+		}
+		_ = closeConn(t, n)
 	}
-	if e != EHDRLEN {
-		t.Fatalf("Expected [%v], got [%v]\n", EHDRLEN, e)
-	}
-	//
-	bvh := Headers{HK_HOST, "localhost", HK_ACCEPT_VERSION, "3.14159"}
-	conn, e = Connect(n, bvh)
-	if e == nil {
-		t.Fatalf("Expected [%v], got [nil]\n", EBADVERCLI)
-	}
-	if e != EBADVERCLI {
-		t.Fatalf("Expected [%v], got [%v]\n", EBADVERCLI, e)
-	}
-	//
-	ch := check11(TEST_HEADERS)
-	conn, e = Connect(n, ch) // Should be a good connect
-	//
-	_, e = conn.Subscribe(neh)
-	if e == nil {
-		t.Fatalf("Expected [%v], got [nil]\n", EHDRLEN)
-	}
-	if e != EHDRLEN {
-		t.Fatalf("Expected [%v], got [%v]\n", EHDRLEN, e)
-	}
-	//
-	e = conn.Unsubscribe(neh)
-	if e == nil {
-		t.Fatalf("Expected [%v], got [nil]\n", EHDRLEN)
-	}
-	if e != EHDRLEN {
-		t.Fatalf("Expected [%v], got [%v]\n", EHDRLEN, e)
-	}
-	//
-	if conn != nil && conn.Connected() {
-		_ = conn.Disconnect(empty_headers)
-	}
-	_ = closeConn(t, n)
 }
