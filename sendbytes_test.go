ae6a86a748c160c7e4dac62d3aae8ca916be7f46
Guy M. Allard <allard.guy.m@gmail.com>
2017-01-17 15:20:13 -0500

    Rework sendbytes tests.

--- sendbytes_test.go
+++ sendbytes_test.go
@@ -24,50 +24,52 @@ import (
 	Test Send Basiconn, one message.
 */
 func TestSendBytesBasic(t *testing.T) {
-
-	n, _ = openConn(t)
-	ch := check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	mb := []byte("A message")
-	d := tdest("/queue/send.basiconn.01")
-	sh := Headers{HK_DESTINATION, d}
-	e = conn.SendBytes(sh, mb)
-	if e != nil {
-		t.Fatalf("Expected nil error, got [%v]\n", e)
+	for _, sp := range Protocols() {
+		n, _ = openConn(t)
+		ch := login_headers
+		ch = headersProtocol(ch, sp)
+		conn, _ = Connect(n, ch)
+		//
+		mb := []byte("A message-" + sp)
+		d := tdest("/queue/send.basiconn.01.bytes." + sp)
+		sh := Headers{HK_DESTINATION, d}
+		e = conn.SendBytes(sh, mb)
+		if e != nil {
+			t.Fatalf("Expected nil error, got [%v]\n", e)
+		}
+		//
+		e = conn.SendBytes(empty_headers, mb)
+		if e == nil {
+			t.Fatalf("Expected error, got [nil]\n")
+		}
+		if e != EREQDSTSND {
+			t.Fatalf("Expected [%v], got [%v]\n", EREQDSTSND, e)
+		}
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 	}
-	//
-	e = conn.SendBytes(empty_headers, mb)
-	if e == nil {
-		t.Fatalf("Expected error, got [nil]\n")
-	}
-	if e != EREQDSTSND {
-		t.Fatalf("Expected [%v], got [%v]\n", EREQDSTSND, e)
-	}
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
-
 }
 
 /*
 	Test Send Multiple, multiple messages, 5 to be exact.
 */
 func TestSendBytesMultiple(t *testing.T) {
-
-	n, _ = openConn(t)
-	ch := check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	mdb := multi_send_data{conn: conn,
-		dest:  tdest("/queue/sendmultiple.01."),
-		mpref: "sendmultiple.01.message.prefix ",
-		count: 5}
-	e = sendMultipleBytes(mdb)
-	if e != nil {
-		t.Fatalf("Expected nil error, got [%v]\n", e)
+	for _, sp := range Protocols() {
+		n, _ = openConn(t)
+		ch := login_headers
+		ch = headersProtocol(ch, sp)
+		conn, _ = Connect(n, ch)
+		//
+		mdb := multi_send_data{conn: conn,
+			dest:  tdest("/queue/sendmultiple.01.bytes." + sp + "."),
+			mpref: "sendmultiple.01.message.prefix.bytes ",
+			count: 5}
+		e = sendMultipleBytes(mdb)
+		if e != nil {
+			t.Fatalf("Expected nil error, got [%v]\n", e)
+		}
+		//
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 	}
-	//
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
-
 }
