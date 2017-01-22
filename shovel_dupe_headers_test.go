1a0296ff24bc22369279b7c30ee22e0e73733d0f
Guy M. Allard <allard.guy.m@gmail.com>
2017-01-21 23:34:48 -0500

    Rework of the test shovel, duplicate headers:
    This need to be fleshed out in the face of all brokers
    we test with.  Firthermore Artemis behavior appears to
    be out of spec.  If verified, a ticket needs to be
    opened.

--- shovel_dupe_headers_test.go
+++ shovel_dupe_headers_test.go
@@ -17,95 +17,128 @@
 package stompngo
 
 import (
-	"os"
+	"fmt"
 	"testing"
 )
 
 /*
-	Test a Stomp 1.1+ shovel.
+	Test a Stomp 1.1+ duplicate header shovel.
+	The Stomp 1.0 specification level is silent on the subject of duplicate
+	headers.  For STOMP 1.1+ this client package is strictly compliant: if user
+	logic passes duplicates, they are sent to the broker.
+	The STOMP 1.1 and 1.2 specifications:
+	a) differ slightly in specified broker behavior
+	b) allow quite a bit of variance in broker behavoir.
+	As usual: YMMV.
 */
-func TestShovel11(t *testing.T) {
-	if os.Getenv("STOMP_TEST11p") == "" || os.Getenv("STOMP_TEST11p") == "1.0 " {
-		t.Skip("Test11Shovel norun, need 1.1+")
-	}
-
-	n, _ = openConn(t)
-	ch := check11(TEST_HEADERS)
-	conn, _ = Connect(n, ch)
-	//
-	ms := "A message"
-	d := tdest("/queue/subunsub.shovel.01")
-	sh := Headers{HK_DESTINATION, d,
-		"dupkey1", "value0",
-		"dupkey1", "value1",
-		"dupkey1", "value2"}
-	_ = conn.Send(sh, ms)
-	//
-	sbh := Headers{HK_DESTINATION, d, HK_ID, d}
-	sc, e = conn.Subscribe(sbh)
-	if e != nil {
-		t.Fatalf("Expected no subscribe error, got [%v]\n", e)
-	}
-	if sc == nil {
-		t.Fatalf("Expected subscribe channel, got [nil]\n")
-	}
-
-	// Read MessageData
-	var md MessageData
-	select {
-	case md = <-sc:
-	case md = <-conn.MessageData:
-		t.Fatalf("read channel error:  expected [nil], got: [%v]\n",
-			md.Message.Command)
-	}
-
-	//
-	if md.Error != nil {
-		t.Fatalf("Expected no message data error, got [%v]\n", md.Error)
-	}
-	rm := md.Message
-	rd := rm.Headers.Value(HK_DESTINATION)
-	if rd != d {
-		t.Fatalf("Expected destination [%v], got [%v]\n", d, rd)
-	}
-	rs := rm.Headers.Value(HK_SUBSCRIPTION)
-	if rs != d {
-		t.Fatalf("Expected subscription [%v], got [%v]\n", d, rs)
-	}
-
-	// Broker behavior can differ WRT repeated header entries
-	// they receive.  Here we try to adjust to observed broker behavior
-	// with the brokers used in local testing.
-	// Also note that the wording of the 1.1 and 1.2 specs is slightly
-	// different WRT repeated header entries.
-	// In any case: YMMV.
-
-	if os.Getenv("STOMP_ARTEMIS") != "" && conn.Protocol() == SPL_11 {
-		if !rm.Headers.ContainsKV("dupkey1", "value2") {
-			t.Fatalf("ART11 Expected true for [%v], [%v]\n", "dupkey1", "value2")
+func TestShovelDupeHeaders(t *testing.T) {
+	for _, sp := range oneOnePlusProtos {
+		n, _ = openConn(t)
+		ch := login_headers
+		ch = headersProtocol(ch, sp)
+		conn, _ = Connect(n, ch)
+		//
+		ms := "A message"
+		d := tdest("/queue/subunsub.shovel.01")
+		sh := Headers{HK_DESTINATION, d}
+		sh = sh.AddHeaders(tsdhHeaders)
+		_ = conn.Send(sh, ms)
+		//
+		sbh := Headers{HK_DESTINATION, d, HK_ID, d}
+		sc, e = conn.Subscribe(sbh)
+		if e != nil {
+			t.Fatalf("Expected no subscribe error, got [%v]\n", e)
 		}
-	} else {
-		if !rm.Headers.ContainsKV("dupkey1", "value0") {
-			t.Fatalf("OTHERs Expected true for [%v], [%v]\n", "dupkey1", "value0")
+		if sc == nil {
+			t.Fatalf("Expected subscribe channel, got [nil]\n")
 		}
-	}
 
-	// Some servers MAY do this.  Apollo is one that does.
-	if os.Getenv("STOMP_APOLLO") != "" {
-		if !rm.Headers.ContainsKV("dupkey1", "value1") {
-			t.Fatalf("APO1 Expected true for [%v], [%v]\n", "dupkey1", "value1")
+		// Read MessageData
+		var md MessageData
+		select {
+		case md = <-sc:
+		case md = <-conn.MessageData:
+			t.Fatalf("read channel error:  expected [nil], got: [%v]\n",
+				md.Message.Command)
 		}
-		if !rm.Headers.ContainsKV("dupkey1", "value2") {
-			t.Fatalf("APO2 Expected true for [%v], [%v]\n", "dupkey1", "value2")
+
+		//
+		if md.Error != nil {
+			t.Fatalf("Expected no message data error, got [%v]\n", md.Error)
 		}
+		rm := md.Message
+		fmt.Printf("SDHT01: %s <%v>\n", conn.Protocol(), rm.Headers)
+		rd := rm.Headers.Value(HK_DESTINATION)
+		if rd != d {
+			t.Fatalf("Expected destination [%v], got [%v]\n", d, rd)
+		}
+		rs := rm.Headers.Value(HK_SUBSCRIPTION)
+		if rs != d {
+			t.Fatalf("Expected subscription [%v], got [%v]\n", d, rs)
+		}
+
+		// Broker behavior can differ WRT repeated header entries
+		// they receive.  Here we try to adjust to observed broker behavior
+		// with the brokers used in local testing.
+		// Also note that the wording of the 1.1 and 1.2 specs is slightly
+		// different WRT repeated header entries.
+		// In any case: YMMV.
+		_ = setTestBroker() // Set brokerid
+
+		// TODO: Fix this per observed behavior
+		switch brokerid {
+		case TEST_AMQ:
+			if !rm.Headers.ContainsKV("dupkey1", "value0") {
+				t.Fatalf("MAIN Expected true for [%v], [%v]\n", "dupkey1", "value0")
+			}
+		case TEST_RMQ:
+			break // For now
+		case TEST_APOLLO:
+			if !rm.Headers.ContainsKV("dupkey1", "value0") {
+				t.Fatalf("MAIN Expected true for [%v], [%v]\n", "dupkey1", "value0")
+			}
+			e = checkDupeHeaders(rm.Headers, wantedDupeVAll)
+			if e != nil {
+				t.Fatalf("Expedted dupe headers, but something is missing: %v %v\n",
+					rm.Headers, wantedDupeVAll)
+			}
+		case TEST_ARTEMIS:
+			// OPen a tciket I think.  This is out of spec.
+			t.Fatalf("ARTEMIS PLAIN Spec Compliant\n")
+			//if !rm.Headers.ContainsKV("dupkey1", "value2") {
+			//	t.Fatalf("MAIN Expected true for [%v], [%v]\n", "dupkey1", "value0")
+			//}
+		default:
+		}
+		/*
+			if os.Getenv("STOMP_ARTEMIS") != "" && conn.Protocol() == SPL_11 {
+				if !rm.Headers.ContainsKV("dupkey1", "value2") {
+					t.Fatalf("ART11 Expected true for [%v], [%v]\n", "dupkey1", "value2")
+				}
+			} else {
+				if !rm.Headers.ContainsKV("dupkey1", "value0") {
+					t.Fatalf("OTHERs Expected true for [%v], [%v]\n", "dupkey1", "value0")
+				}
+			}
+
+			// Some servers MAY do this.  Apollo is one that does.
+			if os.Getenv("STOMP_APOLLO") != "" {
+				if !rm.Headers.ContainsKV("dupkey1", "value1") {
+					t.Fatalf("APO1 Expected true for [%v], [%v]\n", "dupkey1", "value1")
+				}
+				if !rm.Headers.ContainsKV("dupkey1", "value2") {
+					t.Fatalf("APO2 Expected true for [%v], [%v]\n", "dupkey1", "value2")
+				}
+			}
+		*/
+		//
+		uh := Headers{HK_ID, rs, HK_DESTINATION, d}
+		e = conn.Unsubscribe(uh)
+		if e != nil {
+			t.Fatalf("Expected no unsubscribe error, got [%v]\n", e)
+		}
+		//
+		_ = conn.Disconnect(empty_headers)
+		_ = closeConn(t, n)
 	}
-	//
-	uh := Headers{HK_ID, rs, HK_DESTINATION, d}
-	e = conn.Unsubscribe(uh)
-	if e != nil {
-		t.Fatalf("Expected no unsubscribe error, got [%v]\n", e)
-	}
-	//
-	_ = conn.Disconnect(empty_headers)
-	_ = closeConn(t, n)
 }
